4a0735fb5c8f58c130410c9293da522b1114cf91
ikechukwumichael <mikeikechi3@gmail.com>
2020-06-16 07:47:12 +1300

    created dberror.php to output database connection error

---
+++ dberror.php
@@ -0,0 +1,22 @@
+<?php 
+ // include('security.php');
+ include('inc/header.php');
+?>
+       <div class="container">
+        <div class= "row">
+         <div class="col-md-6 mr-auto ml-auto text-center py-5 mt-5">
+           <div class="card">
+             <div class="card-body">
+              <h1 class="card-title bg-danger text-white">Database Connection Failed</h1>
+                <h2 class="card-title"> Database Failure</h2>
+                 <p class="card-text"> Please Check Your Database Connection.</p>
+                 <a href="#" class="btn btn-danger"> :( </a>
+             </div>
+           </div>
+         </div>
+        </div>
+       </div> 
+<?php   
+include('inc/scripts.php');
+// include('inc/footer.php');
+?>
